a5395d974a110944528dcb31b7369cc0fcaaa524
kouen <mcrleo@foxmail.com>
2024-11-12 00:47:13 +0800

    Site updated: 2024-11-12 00:47:12

---
+++ img/webpconv.py
@@ -0,0 +1,23 @@
+import os
+import webp
+from PIL import Image
+
+def main(directory):
+    if not os.path.isdir(directory):
+        print("The directory doesn't exist")
+        return
+
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+            try:
+                im = Image.open(file_path)
+                webp_path = os.path.splitext(file_path)[0] + '.webp'
+                webp.save_image(im, webp_path, quality=70) # quality
+                print(f"{filename} -> {webp_path}")
+            except Exception as e:
+                print(f"Error happened when process {filename}: {e}")
+
+if __name__ == "__main__":
+    directory = input("Directory: ")
+    main()
